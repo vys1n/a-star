addb2b519723f952db8aafcdeedd61dfe85c4b6e
vys1n <kavysingh04@gmail.com>
2026-06-14 00:00:07 +0530

    done

--- main.cpp
+++ main.cpp
@@ -1,22 +1,24 @@
 #include <array>
 #include <cmath>
+#include <cstddef>
 #include <cstdlib>
 #include <limits>
+#include <queue>
 #include <random>
 #include <raylib.h>
-#include <cstddef>
+#include <vector>
 
 class AStar {
 public:
-    static constexpr size_t NODES_ROW_AMOUNT { 20 };
-    static constexpr size_t NODES_PX_SIZE { 50 };
-    static constexpr size_t OBSTACLES_AMT { NODES_ROW_AMOUNT * 5 };
+    static constexpr size_t NODES_ROW_AMOUNT{ 20 * 5 };
+    static constexpr size_t NODES_PX_SIZE{ 50 / 5 };
+    static constexpr size_t OBSTACLES_AMT{ NODES_ROW_AMOUNT * 20 };
 
 public:
     AStar() {
-        for (int y {}; y < NODES_ROW_AMOUNT; y++) {
-            for (int x {}; x < NODES_ROW_AMOUNT; x++) {
-                Node node { x, y };
+        for (int y{}; y < NODES_ROW_AMOUNT; y++) {
+            for (int x{}; x < NODES_ROW_AMOUNT; x++) {
+                Node node{x, y};
                 grid[y][x] = node;
             }
         }
@@ -28,14 +30,18 @@ public:
         start_node->h_cost = heuristic(start_node, end_node);
         start_node->f_cost = start_node->h_cost;
 
-        std::mt19937 generator { std::random_device()() };
-        std::uniform_int_distribution<int> range { 0, NODES_ROW_AMOUNT - 1};
+        start_node->is_open = true;
 
-        for (size_t i {}; i < OBSTACLES_AMT;) {
-            int x { range(generator) };
-            int y { range(generator) };
+        opened.push({ start_node });
 
-            auto* node { get_node(x, y) };
+        std::mt19937 generator{std::random_device()()};
+        std::uniform_int_distribution<int> range{0, NODES_ROW_AMOUNT - 1};
+
+        for (size_t i{}; i < OBSTACLES_AMT;) {
+            int x{range(generator)};
+            int y{range(generator)};
+
+            auto *node{get_node(x, y)};
 
             if (node == start_node || node == end_node)
                 continue;
@@ -50,19 +56,53 @@ public:
     }
 
     void update() {
+        if (finished) {
+            return;
+        }
+        
+        if (opened.empty()) {
+            finished = true;
+            return;
+        }
+
+        auto current { opened.top() };
+        opened.pop();
+
+        if (current.node->is_closed)
+            return;
+
+        current.node->is_closed = true;
+
+        if (current.node == end_node) {
+            reconstruct_path();
+            finished = true;
+            return;
+        }
+
+        expand_node(current.node);
     }
 
     void draw() {
-        for (int y {}; y < NODES_ROW_AMOUNT; y++) {
-            for (int x {}; x < NODES_ROW_AMOUNT; x++) {
-                const auto* node { get_node(x, y) };
-                
-                Color color { GRAY };
+        for (int y{}; y < NODES_ROW_AMOUNT; y++) {
+            for (int x{}; x < NODES_ROW_AMOUNT; x++) {
+                const auto *node{get_node(x, y)};
+                Color color{GRAY};
+
                 if (node->is_obstacle)
                     color = BLACK;
 
+                if (node->is_open)
+                    color = ORANGE;
+                
+                if (node->is_closed)
+                    color = BLUE;
+
                 DrawRectangle(x * NODES_PX_SIZE, y * NODES_PX_SIZE, NODES_PX_SIZE, NODES_PX_SIZE, color);
             }
+        }
+
+        for (const auto* node : final_path) {
+            DrawRectangle(node->x * NODES_PX_SIZE, node->y * NODES_PX_SIZE, NODES_PX_SIZE, NODES_PX_SIZE, YELLOW);
         }
 
         DrawRectangle(start_node->x * NODES_PX_SIZE, start_node->y * NODES_PX_SIZE, NODES_PX_SIZE, NODES_PX_SIZE, GREEN);
@@ -71,51 +111,112 @@ public:
 
 private:
     struct Node {
-        int x {};
-        int y {};
+        int x{};
+        int y{};
 
-        float h_cost {};
-        float g_cost { std::numeric_limits<float>::max() };
-        float f_cost { std::numeric_limits<float>::max() };
+        float h_cost{};
+        float g_cost{std::numeric_limits<float>::max()};
+        float f_cost{std::numeric_limits<float>::max()};
 
-        bool is_obstacle {};
+        bool is_obstacle{};
+        bool is_open {};
+        bool is_closed {};
+
+        Node* parent {};
+    };
+
+    struct OpenNode {
+        Node* node {};
+
+        bool operator<(const OpenNode& other) const {
+            return node->f_cost > other.node->f_cost;
+        }
     };
 
 private:
-    float heuristic(const Node* a, const Node* b) {
+    float heuristic(const Node *a, const Node *b) {
         return std::abs(a->y - b->y) + std::abs(a->x - b->x);
     }
 
     Node* get_node(int x, int y) {
         if (x < 0 || y < 0)
             return nullptr;
-        
-        if (x >= NODES_ROW_AMOUNT || y >= NODES_ROW_AMOUNT )
+
+        if (x >= NODES_ROW_AMOUNT || y >= NODES_ROW_AMOUNT)
             return nullptr;
 
         return &grid[y][x];
     }
 
+    void expand_node(Node* node) {
+        for (auto* neighbour : get_neighbours(node)) {
+            if (neighbour->is_obstacle)
+                continue;
+
+            const float new_g_cost { node->g_cost + 1 };
+        
+            if (new_g_cost >= neighbour->g_cost)
+                continue;
+
+            neighbour->g_cost = new_g_cost;
+            neighbour->h_cost = heuristic(neighbour, end_node);
+            neighbour->f_cost = neighbour->h_cost + neighbour->g_cost;
+
+            neighbour->is_open = true;
+
+            neighbour->parent = node;
+
+            opened.push({ neighbour });
+        }
+    }
+
+    std::vector<Node*> get_neighbours(Node* node) {
+        std::vector<Vector2> directions { { 0 , 1}, { 1, 0 }, { 0 , -1}, { -1 , 0 } };
+        std::vector<Node*> output {};
+        for (const auto& direction : directions) {
+            auto* possible_neighbour { get_node(node->x + direction.x , node->y + direction.y) };
+            if (!possible_neighbour)
+                continue;
+            output.push_back(possible_neighbour);
+        }
+
+        return output;
+    }
+
+    void reconstruct_path() {
+        auto* current { end_node };
+        while (current->parent) {
+            final_path.push_back(current);
+            current = current->parent;
+        }
+        final_path.push_back(current);
+    }
+
 private:
-    std::array<std::array<Node, NODES_ROW_AMOUNT>, NODES_ROW_AMOUNT> grid {};
-    Node* start_node {};
-    Node* end_node {};
+    std::array<std::array<Node, NODES_ROW_AMOUNT>, NODES_ROW_AMOUNT> grid{};
+    std::priority_queue<OpenNode> opened {};
+    std::vector<Node*> final_path {};
+
+    bool finished {};
+
+    Node *start_node{};
+    Node *end_node{};
 };
 
 int main() {
     InitWindow(1000, 1000, "a-star-algorithm");
-    SetTargetFPS(60);
+    SetTargetFPS(600);
 
-    AStar a_star {};
+    AStar a_star{};
 
-    while(!WindowShouldClose()) {
+    while (!WindowShouldClose()) {
         PollInputEvents();
         a_star.update();
 
         BeginDrawing();
         ClearBackground(BLACK);
         a_star.draw();
-        
+
         EndDrawing();
     }
 
